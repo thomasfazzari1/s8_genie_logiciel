107a7b7e9a2b4b795ef0107604ccb61e104c3e5d
thomasfazzari1 <fazzarithomas5@gmail.com>
2024-05-27 18:23:05 +0200

    Classe DBConfigurationTest - Test connexion et fermeture

---
+++ src/test/java/DBConfigurationTest.java
@@ -0,0 +1,25 @@
+import com.mongodb.client.MongoClient;
+import fr.ul.miage.fazzari_chartier_colombana.DBConfiguration;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DBConfigurationTest {
+
+    @Test
+    public void testGetMongoClient() {
+        MongoClient client = DBConfiguration.getMongoClient();
+        assertNotNull(client, "Le client ne doit pas être Null");
+    }
+
+    @Test
+    public void testClose() {
+        MongoClient client = DBConfiguration.getMongoClient();
+        DBConfiguration.close();
+        assertThrows(IllegalStateException.class, () -> {
+            client.getDatabase("test").listCollectionNames().first();
+        }, "IllegalStateException après la fermeture du client");
+    }
+
+}
