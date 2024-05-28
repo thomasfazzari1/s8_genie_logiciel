b3dfa6d529c2bf9857d21a7ea4bb4d6f8e748cbe
thomasfazzari1 <fazzarithomas5@gmail.com>
2024-05-28 11:20:23 +0200

    Opérations basiques des entités + Interfaces

--- src/main/java/fr/ul/miage/fazzari_chartier_colombana/DB/DBBorne.java
+++ src/main/java/fr/ul/miage/fazzari_chartier_colombana/DB/DBBorne.java
@@ -1,26 +1,39 @@
 package fr.ul.miage.fazzari_chartier_colombana.DB;
 
-import fr.ul.miage.fazzari_chartier_colombana.Interfaces.IDBEntite;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import fr.ul.miage.fazzari_chartier_colombana.Interfaces.IDBBorne;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
-public class DBBorne implements IDBEntite {
+import java.util.ArrayList;
+
+public class DBBorne implements IDBBorne {
+    private static MongoDatabase database = DBConfiguration.getDatabase();
+    private static MongoCollection<Document> bornes = database.getCollection("Bornes");
 
     @Override
-    public void ajouter() {
-
+    public void ajouter(int id, String emplacement) {
+        bornes.insertOne(new Document()
+                .append("Id", id)
+                .append("Emplacement", emplacement)
+        );
     }
 
     @Override
-    public void sauvegarder() {
-
+    public void supprimer(int id) {
+        bornes.deleteOne(new Document("Id", id));
     }
 
     @Override
-    public void modifier() {
-
+    public ArrayList<Document> getBornes() {
+        return bornes.find().into(new ArrayList<>());
     }
 
     @Override
-    public void supprimer() {
-
+    public boolean existe(int id) {
+        Bson filter = Filters.eq("Id", id);
+        return bornes.find(filter).first() != null;
     }
 }
--- src/main/java/fr/ul/miage/fazzari_chartier_colombana/DB/DBClient.java
+++ src/main/java/fr/ul/miage/fazzari_chartier_colombana/DB/DBClient.java
@@ -1,25 +1,43 @@
 package fr.ul.miage.fazzari_chartier_colombana.DB;
 
-import fr.ul.miage.fazzari_chartier_colombana.Interfaces.IDBEntite;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import fr.ul.miage.fazzari_chartier_colombana.Interfaces.IDBClient;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
-public class DBClient implements IDBEntite {
+import java.util.ArrayList;
+
+public class DBClient implements IDBClient {
+    private static MongoDatabase database = DBConfiguration.getDatabase();
+    private static MongoCollection<Document> clients = database.getCollection("Clients");
+
     @Override
-    public void ajouter() {
-
+    public void ajouter(String nom, String prenom, String adresse, String telephone, String email, String cb) {
+        clients.insertOne(new Document()
+                .append("Nom", nom)
+                .append("Prenom", prenom)
+                .append("Adresse", adresse)
+                .append("NumeroTelephone", telephone)
+                .append("Email", email)
+                .append("NumeroCB", cb)
+        );
     }
 
     @Override
-    public void sauvegarder() {
-
+    public void supprimer(String email) {
+        clients.deleteOne(new Document("Email", email));
     }
 
     @Override
-    public void modifier() {
-
+    public ArrayList<Document> getClients() {
+        return clients.find().into(new ArrayList<>());
     }
 
     @Override
-    public void supprimer() {
-
+    public boolean existe(String email) {
+        Bson filter = Filters.eq("Email", email);
+        return clients.find(filter).first() != null;
     }
 }
