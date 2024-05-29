6caf33a5a970b9202e6110129ac8de25c12cb576
thomasfazzari1 <fazzarithomas5@gmail.com>
2024-05-29 16:54:39 +0200

    Implémentation de la méthode + Révision des tests (prise en compte du while et simulation de la saisie)

--- src/test/java/GestionnaireBorneTest.java
+++ src/test/java/GestionnaireBorneTest.java
@@ -12,125 +12,102 @@ import java.io.PrintStream;
 
 @DisplayName("On teste si...")
 public class GestionnaireBorneTest {
+    private void setEntreeSortie(String input, ByteArrayOutputStream outputStream) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    private void testerSaisie(String saisie, String messageAttendu) {
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        setEntreeSortie(saisie, contenuSortie);
+
+        Thread testThread = new Thread(() -> {
+            try {
+                GestionnaireBorne.ajouterBorne();
+            } catch (Exception e) {
+            }
+        });
+        testThread.start();
+
+        try {
+            Thread.sleep(1000); // On laisse le temps au thread de lire l'entrée et d'afficher le message
+            testThread.interrupt(); // Simulation de l'arrêt de la saisie
+            testThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains(messageAttendu));
+    }
+
+    // region ajouterBorne
     @Test
     @DisplayName("La saisie d'un ID vide est gérée")
     public void testIDVide() {
-        String saisie = "\n\nEmplacementTest";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireBorne.ajouterBorne();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ Veuillez saisir un ID valide (nombre entier supérieur à 0)."));
+        testerSaisie("\n\n", "❌ Veuillez saisir un ID valide (nombre entier supérieur à 0).");
     }
 
     @Test
     @DisplayName("La saisie d'un ID négatif est gérée")
     public void testIDNegatif() {
-        String saisie = "-4\n\nEmplacementTest";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireBorne.ajouterBorne();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ Veuillez saisir un ID valide (nombre entier supérieur à 0)."));
+        testerSaisie("-1\n-1\n", "❌ Veuillez saisir un ID valide (nombre entier supérieur à 0).");
     }
 
     @Test
     @DisplayName("La saisie d'un ID invalide est gérée")
     public void testIDInvalide() {
-        String saisie = "ChaineDeCaractère\n\nEmplacementTest";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireBorne.ajouterBorne();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ Veuillez saisir un ID valide (nombre entier supérieur à 0)."));
+        testerSaisie("abc\nabc\n", "❌ Veuillez saisir un ID valide (nombre entier supérieur à 0).");
     }
 
     @Test
     @DisplayName("La saisie d'un emplacement vide est gérée")
     public void testEmplacementVide() {
-        String saisie = "1\n\n";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireBorne.ajouterBorne();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ L'emplacement de la borne ne peut pas être vide."));
+        testerSaisie("1\n\n\n", "❌ L'emplacement de la borne ne peut pas être vide.");
     }
 
     @Test
     @DisplayName("La saisie d'un ID dépassant la limite supérieure du type Integer est gérée")
     public void testDepassementLimiteSupID() {
-        long idDepassantMax = (long) Integer.MAX_VALUE + 1;
-        String saisie = idDepassantMax + "\nEmplacementTest";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireBorne.ajouterBorne();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ Veuillez saisir un ID compris entre les limites."));
+        testerSaisie("2147483648\n2147483648\n", "❌ Veuillez saisir un ID compris entre les limites.");
     }
 
     @Test
     @DisplayName("La saisie d'un ID dépassant la limite inférieure du type Integer est gérée")
     public void testDepassementLimiteInfID() {
-        long idDepassantMax = (long) Integer.MIN_VALUE - 1;
-        String saisie = idDepassantMax + "\nEmplacementTest";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireBorne.ajouterBorne();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ Veuillez saisir un ID compris entre les limites."));
+        testerSaisie("-2147483649\n-2147483649\n", "❌ Veuillez saisir un ID compris entre les limites.");
     }
 
     @Test
     @DisplayName("L'ajout d'une borne fonctionne")
     public void testAjoutBorne() {
-        String saisie = "12345\nEmplacement Valide";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
+        DBBorne bornes = new DBBorne();
+
+        String saisie = "999999999\nEmplacement Valide";
         ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
+        setEntreeSortie(saisie, contenuSortie);
+
         GestionnaireBorne.ajouterBorne();
+
         System.setIn(System.in);
         System.setOut(System.out);
+
         String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("✅ Borne ajouté avec succès."));
+        assertTrue(sortie.contains("✅ Borne ajoutée avec succès."));
+
+        bornes.supprimer(999999999);
     }
 
     @Test
-    @DisplayName("L'ajout d'une borne déjà existante fonctionne")
+    @DisplayName("L'ajout d'une borne déjà existante est géré")
     public void testAjoutBorneExistante() {
         DBBorne bornes = new DBBorne();
         bornes.ajouter(123, "Test");
 
         String saisie = "123\nEmplacement Test";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-
         ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
+        setEntreeSortie(saisie, contenuSortie);
 
         GestionnaireBorne.ajouterBorne();
 
@@ -139,5 +116,8 @@ public class GestionnaireBorneTest {
 
         String sortie = contenuSortie.toString();
         assertTrue(sortie.contains("❌ Une borne avec cet identifiant est déjà enregistrée."));
+
+        bornes.supprimer(123);
     }
+    // endregion
 }
