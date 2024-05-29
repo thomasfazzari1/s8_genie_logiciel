3e61383b8e688bb2822a247776fac18c677bd33a
thomasfazzari1 <fazzarithomas5@gmail.com>
2024-05-29 12:35:53 +0200

    Merge US22-menu

--- src/main/java/fr/ul/miage/fazzari_chartier_colombana/Main.java
+++ src/main/java/fr/ul/miage/fazzari_chartier_colombana/Main.java
@@ -1,9 +1,60 @@
 package fr.ul.miage.fazzari_chartier_colombana;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        boolean programme = true;
+        String choix;
+        Scanner scanner = new Scanner(System.in);
 
+        while (programme) {
+            afficherMenu();
+            choix = scanner.nextLine();
+            System.out.println();
+            switch (choix) {
+                case "0": System.out.println("Au revoir !"); programme = false; break;
+                case "1": break;
+                case "2": break;
+                case "3": break;
+                case "4": break;
+                case "5": break;
+                case "6": break;
+                case "7": break;
+                case "8": break;
+                case "9": break;
+                case "10": break;
+                case "11": break;
+                case "12": break;
+                case "13": break;
+                default: System.out.println("Saisie incorrecte."); break;
+            }
+        }
+    }
+
+    private static void afficherMenu() {
+        System.out.println("╔══════════════════════════════════════╗");
+        System.out.println("║                 MENU                 ║");
+        System.out.println("╠══════════════════════════════════════╣");
+        System.out.println("║ 1. Afficher la liste des clients     ║");
+        System.out.println("║ 2. Ajouter un client                 ║");
+        System.out.println("║ 3. Supprimer un client               ║");
+        System.out.println("║ ------------------------------------ ║");
+        System.out.println("║ 4. Afficher la liste des véhicules   ║");
+        System.out.println("║ 5. Ajouter un véhicule à un client   ║");
+        System.out.println("║ 6. Supprimer un véhicule à un client ║");
+        System.out.println("║ ------------------------------------ ║");
+        System.out.println("║ 7. Afficher la liste des bornes      ║");
+        System.out.println("║ 8. Rechercher une borne disponible   ║");
+        System.out.println("║ 9. Ajouter une borne                 ║");
+        System.out.println("║ 10. Supprimer une borne              ║");
+        System.out.println("║ ------------------------------------ ║");
+        System.out.println("║ 11. Afficher la liste des contrats   ║");
+        System.out.println("║ 12. Ajouter un contrat               ║");
+        System.out.println("║ 13. Supprimer un contrat             ║");
+        System.out.println("║ ------------------------------------ ║");
+        System.out.println("║ 0. Quitter                           ║");
+        System.out.println("╚══════════════════════════════════════╝");
+        System.out.print("Saisissez votre choix : ");
     }
 }
