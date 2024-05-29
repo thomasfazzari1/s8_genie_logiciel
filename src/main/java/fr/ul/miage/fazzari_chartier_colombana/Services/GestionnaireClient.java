27bc461876d6ca4f7075c67e6248c434a73f766f
Guillaume540 <115690139+Guillaume540@users.noreply.github.com>
2024-05-29 14:25:07 +0200

    Ecriture des premiers tests de GestionnaireClient

--- src/main/java/fr/ul/miage/fazzari_chartier_colombana/Services/GestionnaireClient.java
+++ src/main/java/fr/ul/miage/fazzari_chartier_colombana/Services/GestionnaireClient.java
@@ -1,4 +1,7 @@
 package fr.ul.miage.fazzari_chartier_colombana.Services;
 
 public class GestionnaireClient {
+    public static void AjouterClient() {
+
+    }
 }
---
+++ src/test/java/GestionnaireClientTest.java
@@ -0,0 +1,111 @@
+import static org.junit.Assert.*;
+
+import fr.ul.miage.fazzari_chartier_colombana.Services.GestionnaireClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+@DisplayName("On teste si...")
+public class GestionnaireClientTest {
+    @Test @DisplayName("L'ajout d'un client fonctionne")
+    public void testClient() {
+        String saisie = "Chartier\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234";
+        InputStream in = new ByteArrayInputStream(saisie.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(contenuSortie));
+        GestionnaireClient.AjouterClient();
+        System.setIn(System.in);
+        System.setOut(System.out);
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains("✅ Client ajouté avec succès."));
+    }
+
+    @Test @DisplayName("La saisie d'un nom vide est gérée")
+    public void testNomClientNonVide() {
+        String saisie = "\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234";
+        InputStream in = new ByteArrayInputStream(saisie.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(contenuSortie));
+        GestionnaireClient.AjouterClient();
+        System.setIn(System.in);
+        System.setOut(System.out);
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains("❌ Le nom du client ne peut pas être vide."));
+    }
+
+    @Test @DisplayName("La saisie d'un prénom vide est gérée")
+    public void testPrenomClientNonVide() {
+        String saisie = "Chartier\n\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234";
+        InputStream in = new ByteArrayInputStream(saisie.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(contenuSortie));
+        GestionnaireClient.AjouterClient();
+        System.setIn(System.in);
+        System.setOut(System.out);
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains("❌ Le prénom du client ne peut pas être vide."));
+    }
+
+    @Test @DisplayName("la saisie d'une adresse vide est gérée")
+    public void testAdresseClientNonVide() {
+        String saisie = "Chartier\nGuillaume\n\n0666666666\nemail@gmail.com\n1234123412341234";
+        InputStream in = new ByteArrayInputStream(saisie.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(contenuSortie));
+        GestionnaireClient.AjouterClient();
+        System.setIn(System.in);
+        System.setOut(System.out);
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains("❌ L'adresse du client ne peut pas être vide."));
+    }
+
+    @Test @DisplayName("la saisie d'un numéro de téléphone vide et gérée")
+    public void testNumeroTelephoneClientNonVide() {
+        String saisie = "Chartier\nGuillaume\nGrande Rue\n\nemail@gmail.com\n1234123412341234";
+        InputStream in = new ByteArrayInputStream(saisie.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(contenuSortie));
+        GestionnaireClient.AjouterClient();
+        System.setIn(System.in);
+        System.setOut(System.out);
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains("❌ Le numéro de téléphone du client ne peut pas être vide."));
+    }
+
+    @Test @DisplayName("la saisie d'un email vide est gérée")
+    public void testEmailClientNonVide() {
+        String saisie = "Chartier\nGuillaume\nGrande Rue\n0666666666\n\n1234123412341234";
+        InputStream in = new ByteArrayInputStream(saisie.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(contenuSortie));
+        GestionnaireClient.AjouterClient();
+        System.setIn(System.in);
+        System.setOut(System.out);
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains("❌ L'email du client ne peut pas être vide."));
+    }
+
+    @Test @DisplayName("la saisie d'une carte bancaire vide est gérée")
+    public void testCarteBancaireClientNonVide() {
+        String saisie = "Chartier\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n\n";
+        InputStream in = new ByteArrayInputStream(saisie.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(contenuSortie));
+        GestionnaireClient.AjouterClient();
+        System.setIn(System.in);
+        System.setOut(System.out);
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains("❌ La carte bancaire du client ne peut pas être vide."));
+    }
+}
