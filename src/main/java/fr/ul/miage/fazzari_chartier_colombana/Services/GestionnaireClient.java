d15b0bc42cdfb9818cc5ede96637a9cc6112b7ba
Guillaume540 <115690139+Guillaume540@users.noreply.github.com>
2024-06-02 13:46:27 +0200

    Optimisation des tests de saisie.

--- src/main/java/fr/ul/miage/fazzari_chartier_colombana/Services/GestionnaireClient.java
+++ src/main/java/fr/ul/miage/fazzari_chartier_colombana/Services/GestionnaireClient.java
@@ -11,25 +11,53 @@ public class GestionnaireClient {
         clients = new DBClient();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nom : "); String nom = scanner.nextLine();
-        System.out.print("Prénom : "); String prenom = scanner.nextLine();
-        System.out.print("Adresse : "); String adresse = scanner.nextLine();
-        System.out.print("Téléphone : "); String telephone = scanner.nextLine();
-        System.out.print("Email : "); String email = scanner.nextLine();
-        System.out.print("Numéro de carte bancaire : "); String cb = scanner.nextLine();
-
-        if (nom.equals("")) System.out.print("❌ Le nom du client ne peut pas être vide.");
-        else if (prenom.equals("")) System.out.print("❌ Le prénom du client ne peut pas être vide.");
-        else if (adresse.equals("")) System.out.print("❌ L'adresse du client ne peut pas être vide.");
-        else if (telephone.equals("")) System.out.print("❌ Le numéro de téléphone du client ne peut pas être vide.");
-        else if (email.equals("")) System.out.print("❌ L'email du client ne peut pas être vide.");
-        else if (cb.equals("")) System.out.print("❌ La carte bancaire du client ne peut pas être vide.");
-        else if (clients.existe(email)) System.out.println("❌ Cet email est déjà associé à un client.");
-        else {
-            // Après avoir géré l'ajout des véhicule, proposer au client s'il souhaite ajouter son 1er véhicule ici !
-
-            clients.ajouter(nom, prenom, adresse, telephone, email, cb);
-            System.out.println("✅ Client ajouté avec succès.\n");
+        boolean boucle = true; String nom = null;
+        while (boucle) {
+            System.out.print("Nom : ");
+            nom = scanner.nextLine();
+            if (nom.equals("")) System.out.println("❌ Le nom du client ne peut pas être vide.");
+            else boucle = false;
         }
+        boucle = true; String prenom = null;
+        while (boucle) {
+            System.out.print("Prénom : ");
+            prenom = scanner.nextLine();
+            if (prenom.equals("")) System.out.println("❌ Le prénom du client ne peut pas être vide.");
+            else boucle = false;
+        }
+        boucle = true; String adresse = null;
+        while (boucle) {
+            System.out.print("Adresse : ");
+            adresse = scanner.nextLine();
+            if (adresse.equals("")) System.out.println("❌ L'adresse du client ne peut pas être vide.");
+            else boucle = false;
+        }
+        boucle = true; String telephone = null;
+        while (boucle) {
+            System.out.print("Numéro de téléphone : ");
+            telephone = scanner.nextLine();
+            if (telephone.equals("")) System.out.println("❌ Le numéro de téléphone du client ne peut pas être vide.");
+            else boucle = false;
+        }
+        boucle = true; String email = null;
+        while (boucle) {
+            System.out.print("Email : ");
+            email = scanner.nextLine();
+            if (email.equals("")) System.out.println("❌ L'email du client ne peut pas être vide.");
+            else if (clients.existe(email)) System.out.println("❌ Cet email est déjà associé à un client.");
+            else boucle = false;
+        }
+        boucle = true; String cb = null;
+        while (boucle) {
+            System.out.print("Carte bancaire : ");
+            cb = scanner.nextLine();
+            if (cb.equals("")) System.out.println("❌ La carte bancaire du client ne peut pas être vide.");
+            else boucle = false;
+        }
+
+        // Après avoir géré l'ajout des véhicule, proposer au client s'il souhaite ajouter son 1er véhicule ici !
+
+        clients.ajouter(nom, prenom, adresse, telephone, email, cb);
+        System.out.println("✅ Client ajouté avec succès.");
     }
 }
--- src/test/java/GestionnaireClientTest.java
+++ src/test/java/GestionnaireClientTest.java
@@ -12,118 +12,89 @@ import java.io.PrintStream;
 
 @DisplayName("On teste si...")
 public class GestionnaireClientTest {
+    DBClient clients = new DBClient();
+
+    private void setEntreeSortie(String input, ByteArrayOutputStream outputStream) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    private void testerSaisie(String saisie, String messageAttendu) {
+        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
+        setEntreeSortie(saisie, contenuSortie);
+        Thread testThread = new Thread(() -> {
+            try {GestionnaireClient.ajouterClient();}
+            catch (Exception e) {}
+        });
+        testThread.start();
+        try {
+            Thread.sleep(1000);
+            testThread.interrupt();
+            testThread.join();
+        }
+        catch (InterruptedException e) {e.printStackTrace();}
+        String sortie = contenuSortie.toString();
+        assertTrue(sortie.contains(messageAttendu));
+    }
+
     @Test @DisplayName("L'ajout d'un client fonctionne")
     public void testClient() {
-        DBClient clients = new DBClient();
-        String saisie = "Chartier\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireClient.ajouterClient();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("✅ Client ajouté avec succès."));
+        testerSaisie("Chartier\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234", "✅ Client ajouté avec succès.");
         clients.supprimer("email@gmail.com");
     }
 
     @Test @DisplayName("La saisie d'un nom vide est gérée")
     public void testNomClientNonVide() {
-        String saisie = "\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireClient.ajouterClient();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ Le nom du client ne peut pas être vide."));
+        testerSaisie("\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234\n", "❌ Le nom du client ne peut pas être vide.");
     }
 
     @Test @DisplayName("La saisie d'un prénom vide est gérée")
     public void testPrenomClientNonVide() {
-        String saisie = "Chartier\n\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireClient.ajouterClient();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ Le prénom du client ne peut pas être vide."));
+        testerSaisie("Chartier\n\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234\n", "❌ Le prénom du client ne peut pas être vide.");
     }
 
     @Test @DisplayName("la saisie d'une adresse vide est gérée")
     public void testAdresseClientNonVide() {
-        String saisie = "Chartier\nGuillaume\n\n0666666666\nemail@gmail.com\n1234123412341234";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireClient.ajouterClient();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ L'adresse du client ne peut pas être vide."));
+        testerSaisie("Chartier\nGuillaume\n\n0666666666\nemail@gmail.com\n1234123412341234\n", "❌ L'adresse du client ne peut pas être vide.");
     }
 
     @Test @DisplayName("la saisie d'un numéro de téléphone vide et gérée")
     public void testNumeroTelephoneClientNonVide() {
-        String saisie = "Chartier\nGuillaume\nGrande Rue\n\nemail@gmail.com\n1234123412341234";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireClient.ajouterClient();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ Le numéro de téléphone du client ne peut pas être vide."));
+        testerSaisie("Chartier\nGuillaume\nGrande Rue\n\nemail@gmail.com\n1234123412341234\n", "❌ Le numéro de téléphone du client ne peut pas être vide.");
     }
 
     @Test @DisplayName("la saisie d'un email vide est gérée")
     public void testEmailClientNonVide() {
-        String saisie = "Chartier\nGuillaume\nGrande Rue\n0666666666\n\n1234123412341234";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireClient.ajouterClient();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ L'email du client ne peut pas être vide."));
+        testerSaisie("Chartier\nGuillaume\nGrande Rue\n0666666666\n\n1234123412341234\n", "❌ L'email du client ne peut pas être vide.");
     }
 
     @Test @DisplayName("la saisie d'une carte bancaire vide est gérée")
     public void testCarteBancaireClientNonVide() {
-        String saisie = "Chartier\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n\n";
-        InputStream in = new ByteArrayInputStream(saisie.getBytes());
-        System.setIn(in);
-        ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(contenuSortie));
-        GestionnaireClient.ajouterClient();
-        System.setIn(System.in);
-        System.setOut(System.out);
-        String sortie = contenuSortie.toString();
-        assertTrue(sortie.contains("❌ La carte bancaire du client ne peut pas être vide."));
+        testerSaisie("Chartier\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n\n", "❌ La carte bancaire du client ne peut pas être vide.");
     }
 
     @Test @DisplayName("Un email déjà existant est géré")
     public void testEmailDejaExistant() {
-        DBClient clients = new DBClient();
         clients.ajouter("Chartier","Guillaume","Grande Rue","0666666666","email@gmail.com","1234123412341234");
         String saisie = "Chartier\nGuillaume\nGrande Rue\n0666666666\nemail@gmail.com\n1234123412341234";
         InputStream in = new ByteArrayInputStream(saisie.getBytes());
         System.setIn(in);
         ByteArrayOutputStream contenuSortie = new ByteArrayOutputStream();
         System.setOut(new PrintStream(contenuSortie));
-        GestionnaireClient.ajouterClient();
         System.setIn(System.in);
         System.setOut(System.out);
+        Thread testThread = new Thread(() -> {
+            try {GestionnaireClient.ajouterClient();}
+            catch (Exception e) {}
+        });
+        testThread.start();
+        try {
+            Thread.sleep(1000);
+            testThread.interrupt();
+            testThread.join();
+        }
+        catch (InterruptedException e) {e.printStackTrace();}
         String sortie = contenuSortie.toString();
         assertTrue(sortie.contains("❌ Cet email est déjà associé à un client."));
         clients.supprimer("email@gmail.com");
